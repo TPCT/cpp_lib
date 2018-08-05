47150da90c86d8501d2e338241597f345cbf560c
TPCT <eslamjackie@outlook.com>
2018-08-05 02:48:14 -0400

    Update strings.h

--- strings.h
+++ strings.h
@@ -2,20 +2,14 @@
 #include <climits>
 #include <string>
 #include <algorithm>
-#include <vector>
 using namespace std;
-using Matrix =  vector<vector<double>>;
-using Row = vector<double>;
-using string_array = vector<string>;
 using char_array = char*;
+typedef string* sptr;
+typedef struct {sptr array; unsigned size = 0;} String_Array;
 string SubStr(string, unsigned, unsigned);
-string* Split(const string, const string);
-//string *copyarray(string*, unsigned);
-void push_back(string*&, string);
-void Print(Matrix);
-void Print(Row);
-void Print(string_array);
-void Print(char_array);
+String_Array Split(const string, const string);
+void Print(String_Array);
+//void Print(char_array);
 int StrIndexOf(const char*, char);
 int StrIndexOf(const char*, const char*);
 int StrIndexOf(const char*, char, unsigned);
@@ -23,4 +17,7 @@ int StrIndexOf(const char*,const char*, unsigned);
 int StrIndexOf(const char*, char, unsigned, unsigned);
 int StrIndexOf(const char*,const char*, unsigned, unsigned);
 int CharArrLen(const char*);
+void push_back(String_Array&, string);
+unsigned precision(void*, unsigned);
+unsigned Number_Length(double);
 size_t ArrLen(string*&);
